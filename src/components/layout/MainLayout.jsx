f853371eec92d0112058a6f2f41032a4d4678b5c
Kuy Tangkoan <kuytangkoan@gmail.com>
2026-02-20 15:29:34 +0700

    plugin mainlayout with antd

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,31 +1,97 @@
-import { Outlet,Link } from "react-router-dom";
-
-const MainLayout = () => {
-
-    return(
-        <div style={{width: '100%', height: '60px',backgroundColor: "yellow"}}>
-            <div style={{width: '100%',}}>
-                <div>Brand Name</div>
-                <div>
-                    <Link to="/home">Home</Link>
-                    <Link to="/about">About</Link>
-                </div>
-
-                {/* outlet នេះសម្រាប់បង្ហាញ Content Page */}
-                <div style={{backgroundColor: "green", height:"100px"}}>
-                    <Outlet/>
-                </div>
-
-                {/* Footer */}
-                <div style={{ backgroundColor: "red", padding: 10}}>
-                    <h1>Footer</h1>
-                    <div>Facebook</div>
-                    <div>Youtube</div>
-                    <div>TikTok</div>
-                </div>
-            </div>
-        </div>
-    )
+import React, { useState } from 'react';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Outlet } from 'react-router-dom';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
 }
-
+const items = [
+  getItem('Home', '/home', <PieChartOutlined />),
+  getItem('About', '/about', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+];
+const MainLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }} items={[{ title: 'User' }, { title: 'Bill' }]} />
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Outlet/>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
 export default MainLayout;
+
+
+
+
+// import { Outlet,Link } from "react-router-dom";
+
+// const MainLayout = () => {
+
+//     return(
+//         <div style={{width: '100%', height: '60px',backgroundColor: "yellow"}}>
+//             <div style={{width: '100%',}}>
+//                 <div>Brand Name</div>
+//                 <div>
+//                     <Link to="/home">Home</Link>
+//                     <Link to="/about">About</Link>
+//                 </div>
+
+//                 {/* outlet នេះសម្រាប់បង្ហាញ Content Page */}
+//                 <div style={{backgroundColor: "green", height:"100px"}}>
+//                     <Outlet/>
+//                 </div>
+
+//                 {/* Footer */}
+//                 <div style={{ backgroundColor: "red", padding: 10}}>
+//                     <h1>Footer</h1>
+//                     <div>Facebook</div>
+//                     <div>Youtube</div>
+//                     <div>TikTok</div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default MainLayout;
