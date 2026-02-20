1b83c7533958f47b9d91ffb2e86be32a3b769715
Kuy Tangkoan <kuytangkoan@gmail.com>
2026-02-20 14:41:05 +0700

    install pugin antd

---
+++ Noded/8-react plugin antd.md
@@ -0,0 +1,5 @@
+មេរៀននេះគឺដំឡើង UI ចេញពី Antd Design
+
+// យើង import មកប្រើ
+import { Button } from 'antd';
+<Button type="primary">Primary Button</Button>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,12 @@
+import { Button } from 'antd';
 import './homePage.css'
 const HomePage = () => {
     return(
-        <div>
-            <p>Hello Style inline</p>
+        <div style={{padding: 10, paddingTop: 35}}>
+            <Button type="primary">Primary Button</Button>
         </div>
+
+        
     )
 }
 export default HomePage;
